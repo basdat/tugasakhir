761ff151b7aa8c6da4156ca7ac69373f8e31bde9
Ibrahim <ibamibrahim0@gmail.com>
2016-12-02 00:55:26 +0700

    Fitur 6&7 done, Tinggal handling session

--- _content/root_jadwal_non_sidang_dosen_content.php
+++ _content/root_jadwal_non_sidang_dosen_content.php
@@ -1,3 +1,20 @@
+<?php
+require_once "database.php";
+$db = new database();
+$conn = $db->connectDB();
+
+$stmt_get_all_nama_dosen = $conn->prepare("Select nip, nama from Dosen;");
+$stmt_get_all_nama_dosen->execute(array());
+$allNamaDosen = $stmt_get_all_nama_dosen->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt_get_all_jadwal = $conn->prepare("Select idjadwal, tanggalmulai, tanggalselesai, alasan, repetisi, nama, nipdosen from jadwal_non_sidang jns, dosen d where jns.nipdosen = d.nip;");
+
+$stmt_get_all_jadwal->execute(array());
+$allJadwal = $stmt_get_all_jadwal->fetchAll(PDO::FETCH_ASSOC);
+
+//TODO: Handling dia admin atau dosen
+
+?>
 <section id="hero" class="header">
 	<div class="container">
 		<div class="row">
@@ -22,76 +39,37 @@
 					<thead class="thead-inverse">
 						<tr>
 							<th>No.</th>
+							<th>Nama</th>
 							<th>Tanggal</th>
-							<th>Jam</th>
+							<th>Repetisi</th>
 							<th>Keterangan</th>
 							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody class="table-condensed">
-						<tr>
-							<th>1</th>
-							<td>Sen, 29 Aug 2016 - Jum, 6 Jan 2017</td>
-							<td>08.00 - 17.00</td>
-							<td>Kunjungan Riset ke Australia</td>
-							<td><button type="button" class="btn btn-warning">Edit</button></td>
-						</tr>
-						<tr>
-							<th scope="row">2</th>
-							<td>Rabu, 15 Jun 2016 - Jum, 17 Jun 2016</td>
-							<td>08.00 - 10.00</td>
-							<td>Visitasi BAN PT</td>
-							<td><button type="button" class="btn btn-warning">Edit</button></td>
-						</tr>
-						<tr>
-							<th scope="row">3</th>
-							<td>Jum, 12 Aug 2016 - Jum, 1 Des 2016</td>
-							<td>08.00 - 17.00</td>
-							<td>Konferensi ke Thailand</td>
-							<td><button type="button" class="btn btn-warning">Edit</button></td>
-						</tr>
-						<tr>
-							<th scope="row">1</th>
-							<td>Sen, 29 Aug 2016 - Jum, 6 Jan 2017</td>
-							<td>08.00 - 17.00</td>
-							<td>Kunjungan Riset ke Australia</td>
-							<td><button type="button" class="btn btn-warning">Edit</button></td>
-						</tr>
-						<tr>
-							<th scope="row">2</th>
-							<td>Rabu, 15 Jun 2016 - Jum, 17 Jun 2016</td>
-							<td>08.00 - 10.00</td>
-							<td>Visitasi BAN PT</td>
-							<td><button type="button" class="btn btn-warning">Edit</button></td>
-						</tr>
-						<tr>
-							<th scope="row">3</th>
-							<td>Jum, 12 Aug 2016 - Jum, 1 Des 2016</td>
-							<td>08.00 - 17.00</td>
-							<td>Konferensi ke Thailand</td>
-							<td><button type="button" class="btn btn-warning">Edit</button></td>
-							</tr>
-							<tr>
-								<th scope="row">1</th>
-								<td>Sen, 29 Aug 2016 - Jum, 6 Jan 2017</td>
-								<td>08.00 - 17.00</td>
-								<td>Kunjungan Riset ke Australia</td>
-								<td><button type="button" class="btn btn-warning">Edit</button></td>
-							</tr>
-							<tr>
-								<th scope="row">2</th>
-								<td>Rabu, 15 Jun 2016 - Jum, 17 Jun 2016</td>
-								<td>08.00 - 10.00</td>
-								<td>Visitasi BAN PT</td>
-								<td><button type="button" class="btn btn-warning">Edit</button></td>
-							</tr>
-							<tr>
-								<th scope="row">3</th>
-								<td>Jum, 12 Aug 2016 - Jum, 1 Des 2016</td>
-								<td>08.00 - 17.00</td>
-								<td>Konferensi ke Thailand</td>
-								<td><button type="button" class="btn btn-warning">Edit</button></td>
-							</tr>
+						<?php 
+							$counter = 1;
+							foreach ($allJadwal as $key => $jadwal) {
+								
+								$id = $jadwal['idjadwal'];
+								$nama = $jadwal['nama'];
+								$tglMulai = $jadwal['tanggalmulai'];
+								$tglSelesai = $jadwal['tanggalselesai'];
+								$repetisi = $jadwal['repetisi'];
+								$alasan = $jadwal['alasan'];
+								$nip = $jadwal['nipdosen'];
+
+								echo "<tr>";
+								echo "<td>" . $counter . "</td>";
+								echo "<td>" . $jadwal['nama'] . "</td>";
+								echo "<td>" . $jadwal['tanggalmulai'] . " - " . $jadwal['tanggalselesai'] . "</td>";
+								echo "<td>" . ucwords($jadwal['repetisi']) . "</td>";
+								echo "<td>" . ucwords($jadwal['alasan']) . "</td>";
+								echo "<td><button type='button' class='btn btn-warning' id='" . $jadwal['idjadwal'] . "' onclick='updateData('$id','$nama','$tglMulai','$tglSelesai','$repetisi','$alasan','$nip')'>Edit</button></td>";
+								echo "</tr>";
+								$counter++;
+							}
+						?>
 					</tbody>
 				</table>
 			</table>
@@ -105,43 +83,92 @@
 						<h4 class="modal-title">Tambah Jadwal Non Sidang</h4>
 					</div>
 					<div class="modal-body">
-						<form action="_content\root_jadwal_non_sidang_dosen_content.php" method="post">
+						<form action="helper_jadwal_non_sidang.php" method="post">
 							<div class="form-group">
-								<label for="namaDosen">Nama Dosen: </label>
-								<input type="text" class="form-control" id="namaDosen" name="namaDosen">
+								<label for="namaDosen">Nama Dosen: </label> <br>
+								<select class="form-control" id="sel1" name="nipDosen">
+								    <?php 
+								    foreach ($allNamaDosen as $key => $dosen) {
+								    	echo "<option value='". $dosen['nip'] ."'>";
+								    	echo $dosen['nama'] . " - " . $dosen['nip'];
+								    	echo "</option>";
+								    }
+								    ?>
+								</select>
 							</div>
 							<div class="form-group col-xs-6">
 								<label for="tanggalMulai">Tanggal Mulai: </label>
 								<input type="date" class="form-control" id="tanggalMulai" name="tanggalMulai">
 							</div>
 							<div class="form-group col-xs-6">
-								<label for="tanggalSelesai">Tanggal Mulai: </label>
+								<label for="tanggalSelesai">Tanggal Selesai: </label>
 								<input type="date" class="form-control" id="tanggalSelesai" name="tanggalSelesai">
 							</div>
-							<div class="form-group col-xs-6">
-								<label for="jamMulai">Jam Mulai: </label>
-								<input type="time" class="form-control" id="jamMulai" name="jamMulai">
-							</div>
-							<div class="form-group col-xs-6">
-								<label for="jamSelesai">Jam Selesai: </label>
-								<input type="time" class="form-control" id="jamSelesai" name="jamSelesai">
-							</div>
 							<div class="col-xs-12">
-								Repetisi Kegiatan: <br>
-								<label class="radio-inline"><input type="radio" name="optradio"> Harian&nbsp</label>
-								<label class="radio-inline"><input type="radio" name="optradio"> Mingguan&nbsp</label>
-								<label class="radio-inline"><input type="radio" name="optradio"> Bulanan&nbsp</label>
+								Repetisi Kegiatan:
+								<div class="form-control">
+									<label class="form-control radio-inline"><input type="radio" name="repetisi" value="harian"> Harian&nbsp</label>
+									<label class="form-control radio-inline"><input type="radio" name="repetisi" value="mingguan"> Mingguan&nbsp</label>
+									<label class="form-control radio-inline"><input type="radio" name="repetisi" value="bulanan"> Bulanan&nbsp</label>
+								</div>
 							</div>
 							<div class="form-group">
 								<label for="keterangan">Keterangan: </label>
-								<input type="text" class="form-control" id="keterangan" name="keterangan">
+								<textarea class="form-control" id="keterangan" name="keterangan"></textarea>
 							</div>
-						</form>
+						
 					</div>
 					<div class="modal-footer">
 						<input type="submit" class="btn btn-success" name="simpan" value="Simpan">
 						<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
 					</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div id="modalEdit" class="modal fade" role="dialog">
+			<div class="modal-dialog modal-lg">
+				<!-- Modal content-->
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title" id="editTitle">Edit Jadwal Non Sidang &nbsp &nbsp &nbsp ID Jadwal: <span></span></h4>
+					</div>
+					<div class="modal-body">
+						<form action="helper_jadwal_non_sidang.php" method="post">
+							<div class="form-group">
+								<label for="namaDosen">Nama Dosen: </label> <br>
+								<select class="form-control" id="editNama" name="nipDosen">
+								</select>
+							</div>
+							<div class="form-group col-xs-6">
+								<label for="tanggalMulai">Tanggal Mulai: </label>
+								<input type="date" class="form-control" id="editTanggalMulai" name="tanggalMulai">
+							</div>
+							<div class="form-group col-xs-6">
+								<label for="tanggalSelesai">Tanggal Selesai: </label>
+								<input type="date" class="form-control" id="editTanggalSelesai" name="tanggalSelesai">
+							</div>
+							<div class="col-xs-12">
+								Repetisi Kegiatan:
+								<div class="form-control">
+									<label class="form-control radio-inline"><input type="radio" name="repetisi" value="harian"> Harian&nbsp</label>
+									<label class="form-control radio-inline"><input type="radio" name="repetisi" value="mingguan"> Mingguan&nbsp</label>
+									<label class="form-control radio-inline"><input type="radio" name="repetisi" value="bulanan"> Bulanan&nbsp</label>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="keterangan">Keterangan: </label>
+								<textarea class="form-control" id="editKeterangan" name="keterangan"></textarea>
+							</div>
+						
+					</div>
+					<div class="modal-footer">
+						<input type="submit" class="btn btn-success" name="simpan" value="Simpan">
+						<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+					</div>
+					</form>
 				</div>
 			</div>
 		</div>
@@ -153,5 +180,12 @@
 	src: url('../fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'), url('../fonts/glyphicons-halflings-regular.woff') format('woff'), url('../fonts/glyphicons-halflings-regular.ttf') format('truetype'), url('../fonts/glyphicons-halflings-regular.svg#glyphicons-halflingsregular') format('svg');
 	}
 	</style>
+	<script src="js/vendor/jquery-3.1.1.js"></script>
+	<script src="js/vendor/tether.js"></script>
+	<script>
+		 function updateData(id, nama, tglMulai, tglSelesai, repetisi, alasan, nip) {
+            alert("assad");      
+         }
+	</script>
 </body>
 </html>
