7211b756237a0145eebf419ff8e8d569a0920932
affan.dhia <affandhia@gmail.com>
2016-12-05 23:20:58 +0700

    added index_admin filter full functionality, fixed typo on jadwal_sidang_admin, fix bug for generateTable by create another method.

--- _content/root_index_admin_content.php
+++ _content/root_index_admin_content.php
@@ -26,58 +26,65 @@ $terms = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <div class="row">
                         <ul class="nav nav-tabs">
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Jadwal Sidang</a>
+                                <a class="nav-link active" href="#filterJadwal" role="tab" data-toggle="tab">Jadwal
+                                    Sidang</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Mahasiswa</a>
+                                <a class="nav-link" href="#filterMahasiswa" role="tab" data-toggle="tab">Mahasiswa</a>
                             </li>
                         </ul>
-                        <div class="form-group row">
-                            <label for="inputTerm" class="col-sm-2 form-control-label">Term</label>
-                            <div class="col-sm-10">
-                                <select class="custom-select" id="inputTerm">
-                                    <option selected value="-1,-1">Semua Waktu</option>
-                                    <?php
-                                    foreach ($terms as $term) {
-                                        $semesterId = (int)($term['semester']);
-                                        $semester = "";
-                                        if ($semesterId == 3) {
-                                            $semester = "Semester Pendek";
-                                        } elseif ($semesterId % 2 == 0) {
-                                            $semester = "Genap";
-                                        } else
-                                            $semester = "Ganjil";
+                        <div class="tab-content">
+                            <div class="tab-pane fade in active" role="tabpanel" id="filterJadwal">
+                                <div class="form-group row">
+                                    <label for="inputTerm" class="col-sm-2 form-control-label">Term</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-control" id="inputTerm">
+                                            <option selected value="-1,-1">Semua Waktu</option>
+                                            <?php
+                                            foreach ($terms as $term) {
+                                                $semesterId = (int)($term['semester']);
+                                                $semester = "";
+                                                if ($semesterId == 3) {
+                                                    $semester = "Semester Pendek";
+                                                } elseif ($semesterId % 2 == 0) {
+                                                    $semester = "Genap";
+                                                } else
+                                                    $semester = "Ganjil";
 
-                                        printf("<option value=\"%s,%s\">%s %s</option>", $semesterId, $term['tahun'], $semester, $term['tahun']);
-                                    }
-                                    ?>
-                                </select>
+                                                printf("<option value=\"%s,%s\">%s %s</option>", $semesterId, $term['tahun'], $semester, $term['tahun']);
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputJenis" class="col-sm-2 form-control-label">Jenis Sidang</label>
+                                    <div class="col-sm-10">
+                                        <select id="inputJenis" class="form-control">
+                                            <option value="0" selected>Semua Jenis</option>
+                                            <?php
+                                            foreach ($jenisMKS as $mks) {
+                                                printf("<option value=\"%s\">%s</option>", $mks['id'], $mks['namamks']);
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row text-xs-right">
+                                    <input type="submit" id="btnSubmit" name="submit" class="btn btn-primary btn-login"
+                                           value="Pilih">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="inputJenis" class="col-sm-2 form-control-label">Jenis Sidang</label>
-                            <div class="col-sm-10">
-                                <select id="inputJenis" class="custom-select">
-                                    <option value="0" selected>Semua Jenis</option>
-                                    <?php
-                                    foreach ($jenisMKS as $mks) {
-                                        printf("<option value=\"%s\">%s</option>", $mks['id'], $mks['namamks']);
-                                    }
-                                    ?>
-                                </select>
+                            <div class="tab-pane fade in form-group" role="tabpanel" id="filterMahasiswa">
+                                <label for="inputFilterMahasiswa" class="col-sm-2 form-control-label">Cari
+                                    Mahasiswa</label>
+                                <div class="col-sm-10">
+                                    <input type="search" id="inputFilterMahasiswa"
+                                           class="form-control" placeholder="Nama Mahasiswa">
+                                </div>
                             </div>
-                        </div>
-                        <div class="row text-xs-right">
-                            <input type="submit" id="btnSubmit" name="submit" class="btn btn-primary btn-login" value="Pilih">
                         </div>
                     </div>
-                    <div class="row">
-                        <label for="sort">Sort by</label>
-                        <select style="" id="sort">
-                            <option value="waktu">Waktu</option>
-                            <option value="mahasiswa">Mahasiswa</option>
-                            <option value="jenis_sidang">Jenis Sidang</option>
-                        </select></div>
                     <div id="table_admin">
                         <div class="alert alert-warning">Sedang mengunduh data...</div>
                     </div>
@@ -85,31 +92,27 @@ $terms = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <div class="row">
 
                 </div>
-
-                <!--<div class="row text-xs-center">
-                    <nav aria-label="Page navigation">
-                        <ul class="pagination">
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true">«</span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true">»</span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-
-                </div>-->
                 <script>
                     $(document).ready(function () {
+                        function getTable() {
+                            var dataTable = $('.table').DataTable({
+                                "paging": true,
+                                "ordering": false,
+                                "info": false,
+                                "initComplete": function (nRow, aData, iDataIndex) {
+                                    var html = '&nbsp&nbsp&nbsp<label for="sort">Sort by: &nbsp</label><select id="sort"><option value="waktu" class="input-large" ">Waktu Sidang</option><option value="mahasiswa">Nama Mahasiswa</option><option value="jenis_sidang">Jenis Sidang</option></select></div>';
+
+                                    $('.dataTables_length').append(html);
+                                    $('div.dataTables_wrapper div.dataTables_length select').css('width', '150px');
+                                    $('#table_admin select').addClass("form-control");
+                                    $('#table_admin input').addClass("form-control");
+                                }
+                            });
+
+                            return dataTable;
+                        }
+
+                        var currentTable = getTable();
                         var inputTerm = $('#inputTerm').val();
                         var inputJenis = $('#inputJenis').val();
 
@@ -134,34 +137,52 @@ $terms = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             inputJenis = $('#inputJenis').val();
                         });
 
-                        $('.table').DataTable({
-                            "paging": true,
-                            "ordering": false,
-                            "info": false,
+                        $(document).on("keyup change", "#inputFilterMahasiswa", function () {
+                            if ($("#table_admin tbody tr").children('td').hasClass("dataTables_empty")) {
+                                $("#inputFilterMahasiswa").parent().addClass("has-warning");
+                                $("#inputFilterMahasiswa").focus().addClass("form-control-warning");
+                            } else {
+                                $("#inputFilterMahasiswa").parent().removeClass("has-warning");
+                                $("#inputFilterMahasiswa").focus().removeClass("form-control-warning");
+                            }
                         });
 
                         $('#btnSubmit').click(function () {
-                            $("#table_admin").html("<div class=\"alert alert-warning\">Sedang mengunduh data...</div>");
-                            $.post("server/server_jadwal_sidang_admin.php", {admin_order: 'js.tanggal ASC, js.jammulai ASC', input_term: inputTerm, input_jenis: inputJenis}, function (response) {
+                            $("#table_admin tbody").html("<td colspan='7' class=\"alert alert-warning\">Sedang mengunduh data...</td>");
+                            $.post("server/server_jadwal_sidang_admin.php", {
+                                admin_order: 'js.tanggal ASC, js.jammulai ASC',
+                                input_term: inputTerm,
+                                input_jenis: inputJenis
+                            }, function (response) {
                                 $("#table_admin").html(response);
-                                $('.table').DataTable({
-                                    "paging": true,
-                                    "ordering": false,
-                                    "info": false,
-                                });
+                                getTable();
                             });
                         });
 
-                        $.post("server/server_jadwal_sidang_admin.php", {admin_order: 'js.tanggal ASC, js.jammulai ASC', input_term: inputTerm, input_jenis: inputJenis}, function (response) {
+                        $.post("server/server_jadwal_sidang_admin.php", {
+                            admin_order: 'js.tanggal ASC, js.jammulai ASC',
+                            input_term: inputTerm,
+                            input_jenis: inputJenis
+                        }, function (response) {
                             $("#table_admin").html(response);
-                            $('.table').DataTable({
-                                "paging": true,
-                                "ordering": false,
-                                "info": false,
-                            });
+                            currentTable = getTable();
                         });
 
-                        $("#sort").change(function () {
+                        $(document).on("keyup change", "#table_admin input[type='search']", function () {
+                            if ($("#table_admin tbody tr").children('td').hasClass("dataTables_empty")) {
+                                $("#table_admin input[type='search']").parent().addClass("has-warning");
+                                $("#table_admin input[type='search']").focus().addClass("form-control-warning");
+                            } else {
+                                $("#table_admin input[type='search']").parent().removeClass("has-warning");
+                                $("#table_admin input[type='search']").focus().removeClass("form-control-warning");
+                            }
+                        });
+
+                        $("#inputFilterMahasiswa").on('keyup change', function () {
+                            currentTable.columns(0).search(this.value).draw();
+                        });
+
+                        $(document).on("change", "#sort", function () {
                             var val = $("#sort").val();
                             var order = "";
 
@@ -174,14 +195,14 @@ $terms = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             } else {
                                 order = 'js.tanggal ASC, js.jammulai ASC';
                             }
-                            $("#table_admin").html("<div class=\"alert alert-warning\">Sedang mengunduh data...</div>");
-                            $.post("server/server_jadwal_sidang_admin.php", {admin_order: order, input_term: inputTerm, input_jenis: inputJenis}, function (response) {
+                            $("#table_admin tbody").html("<td colspan='7' class=\"alert alert-warning\">Sedang mengunduh data...</td>");
+                            $.post("server/server_jadwal_sidang_admin.php", {
+                                admin_order: order,
+                                input_term: inputTerm,
+                                input_jenis: inputJenis
+                            }, function (response) {
                                 $("#table_admin").html(response);
-                                $('.table').DataTable({
-                                    "paging": true,
-                                    "ordering": false,
-                                    "info": false,
-                                });
+                                currentTable = getTable();
                             });
                         });
                     });
--- server/server_jadwal_sidang_admin.php
+++ server/server_jadwal_sidang_admin.php
@@ -14,7 +14,85 @@ if (isset($_POST{"edit"})) {
     $_SESSION["edit_idjs"] = $_POST["edit"];
 }
 
-function generateTable($order, $filterTerm = '-1,-1', $filterJenis = -1)
+function generateTable($order)
+{
+    $query = "SELECT js.idjadwal,mks.ijinmajusidang,mks.pengumpulanhardcopy,mh.nama,jm.namamks,mks.judul,js.tanggal,js.jammulai,js.jamselesai,mks.idmks,r.namaruangan
+FROM jadwal_sidang js NATURAL JOIN mata_kuliah_spesial mks NATURAL JOIN mahasiswa mh JOIN jenis_mks jm ON mks.idjenismks = jm.id NATURAL JOIN ruangan r
+WHERE mks.issiapsidang = true
+ORDER BY " . $order;
+    $pdoArray = array();
+
+    $db = new database();
+    $conn = $db->connectDB();
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute($pdoArray);
+    $datas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $html = "<table class='table dataTable'>";
+    $html .= "<colgroup>
+                <col style='width:10%'>
+                <col style='width:10%'>
+                <col style='width:10%'>
+                <col style='width:10%'>
+                <col style='width:10%'>
+                <col style='width:10%'>
+                <col style='width:1%'>
+            </colgroup>";
+    $html .= "<thead><tr>";
+
+    $columnName = array('Mahasiswa', 'Judul', 'Jenis Sidang', 'Waktu dan Lokasi', 'Penguji', 'Pembimbing', 'Action');
+    foreach ($columnName as $th) {
+        $html = $html . "<th>" . $th . " </th>";
+    }
+    $html = $html . "</thead></tr>";
+
+    foreach ($datas as $key => $dataRow) {
+        $html = $html . "<tr>";
+
+        $html = $html . "<td>" . $dataRow['nama'] . "</td>" .
+            "<td>" . $dataRow['judul'] . "</td>" .
+            "<td>" . $dataRow['namamks'];
+
+
+        $waktudanlokasi = "<td>";
+        $waktudanlokasi = $waktudanlokasi . $dataRow['tanggal'] . "\n" . $dataRow['jammulai'] . "-" . $dataRow['jamselesai'] . "\n" . $dataRow['namaruangan'] . "</td>";
+        $html = $html . $waktudanlokasi;
+
+        $dospenghtml = "<td>";
+
+        $stmt = $conn->prepare("SELECT d.nama FROM dosen_penguji dpem JOIN dosen d ON d.nip = dpem.nipdosenpenguji WHERE dpem.idmks=:idmks");
+        $stmt->execute(array(':idmks' => $dataRow['idmks']));
+        $dospeng = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($dospeng as $key => $dataR) {
+            $dospenghtml = $dospenghtml . $dataR['nama'] . "\n";
+        }
+
+        $dospenghtml = $dospenghtml . "</td>";
+
+        $html = $html . $dospenghtml;
+
+        $dospemhtml = "<td>";
+        $stmt = $conn->prepare("SELECT d.nama FROM dosen_pembimbing dpem JOIN dosen d ON d.nip = dpem.nipdosenpembimbing WHERE dpem.idmks=:idmks");
+        $stmt->execute(array(':idmks' => $dataRow['idmks']));
+        $dospem = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($dospem as $key => $dataR2) {
+            $dospemhtml = $dospemhtml . $dataR2['nama'] . "\n";
+        }
+        $dospemhtml = $dospemhtml . "</td>";
+
+        $html = $html . $dospemhtml;
+
+        $html = $html . "<td>.<button id='" . $dataRow['idjadwal'] . "' class='btn btn-primary edit'>Edit</button>.</td>";
+
+        $html = $html . "</tr>";
+    }
+    $html = $html . "</table>";
+    return $html;
+}
+
+function generateTableWithFilter($order, $filterTerm = '-1,-1', $filterJenis = -1)
 {
     $filterSemester = (int)explode(',', $filterTerm)[0];
     $filterTahun = (int)explode(',', $filterTerm)[1];
@@ -115,7 +193,7 @@ ORDER BY ".$order;
 }
 
 if (isset($_POST['input_term'])) {
-    echo generateTable($_POST["admin_order"], $_POST["input_term"], (int)$_POST["input_jenis"]);
+    echo generateTableWithFilter($_POST["admin_order"], $_POST["input_term"], (int)$_POST["input_jenis"]);
 } elseif (isset($_POST["admin_order"])) {
     echo generateTable($_POST["admin_order"]);
 }
